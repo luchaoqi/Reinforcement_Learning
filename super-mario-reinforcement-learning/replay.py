e5434de4d540eb1cc4b2311a621014ed9b10aad1
LuchaoQi <lc.qi@outlook.com>
2020-04-14 11:44:14 -0700

    Luchao's auto-commitment using shell script

--- super-mario-reinforcement-learning/replay.py
+++ super-mario-reinforcement-learning/replay.py
@@ -1,5 +1,6 @@
 from agent import DQNAgent
-from nes_py.wrappers import BinarySpaceToDiscreteSpaceEnv
+# from nes_py.wrappers import BinarySpaceToDiscreteSpaceEnv
+from nes_py.wrappers import JoypadSpace
 import gym_super_mario_bros
 from gym_super_mario_bros.actions import RIGHT_ONLY
 from wrappers import wrapper
@@ -7,7 +8,8 @@ from wrappers import wrapper
 
 # Build env
 env = gym_super_mario_bros.make('SuperMarioBros-1-1-v0')
-env = BinarySpaceToDiscreteSpaceEnv(env, RIGHT_ONLY)
+#env = BinarySpaceToDiscreteSpaceEnv(env, RIGHT_ONLY)
+env = JoypadSpace(env,RIGHT_ONLY)
 env = wrapper(env)
 
 # Parameters
@@ -18,4 +20,6 @@ actions = env.action_space.n
 agent = DQNAgent(states=states, actions=actions, max_memory=100000, double_q=True)
 
 # Replay
-agent.replay(env=env, model_path='./models/final-vm-1', n_replay=1, plot=True)
+model_path = './models'
+# './models/final-vm-1'
+agent.replay(env=env, model_path=model_path, n_replay=1, plot=False)
