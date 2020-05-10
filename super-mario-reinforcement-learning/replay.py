bee7de19d85242a1888001fdf69f100983d992d9
LuchaoQi <lc.qi@outlook.com>
2020-05-09 19:17:11 -0700

    Luchao's auto-commitment using shell script

--- super-mario-reinforcement-learning/replay.py
+++ super-mario-reinforcement-learning/replay.py
@@ -25,7 +25,7 @@ agent = DQNAgent(states=states, actions=actions, max_memory=100000, double_q=Tru
 
 # Replay
 # model_path = '/home/luchao-qi/Downloads/models'
-model_path = './models'
-# model_path = './models/temp_models'
+# model_path = './models'
+model_path = './models/temp_models'
 # './models/final-vm-1'
 agent.replay(env=env, model_path=model_path, n_replay=1, plot=False)
